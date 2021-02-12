a3f17cd297288f5a0acc72fb5b18907e13b72ab6
aka-risu <kyr.anastasiia@gmail.com>
2021-02-12 22:35:32 +0200

    adds classes to pag buttons and adds loader

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -1,5 +1,13 @@
 import loaderTemplate from './loader.hbs';
 import refs from '../js/components/refs';
+import camera from './img/camera.png';
+import film from './img/film.png';
 export default function addLoader() {
-  refs.loaderRef.insertAdjacentHTML('beforeend', loaderTemplate());
+  console.log('Loader');
+  refs.loaderRef.insertAdjacentHTML(
+    'beforeend',
+    loaderTemplate({ camera, film }),
+  );
+  console.log('Loader2');
 }
+//  addLoader;
